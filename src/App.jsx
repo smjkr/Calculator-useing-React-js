30e3a6c8ef8c706e340a2a8c331e0c87e1323885
smjkr <smjakir2010@outlook.com>
2024-04-20 12:55:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import styles from "./App.module.css"
+import Display from "./components/Display.jsx";
+import ButtonContainer from "./components/ButtonsContainer.jsx";
+import {useState} from "react";
+
+function App() {
+    const [calVal, setCalVal] = useState("");
+    const onButtonClick = (buttonText) => {
+        if (buttonText==="C"){
+            setCalVal("")
+        }
+        else if (buttonText==="="){
+            const result = eval(calVal);
+            setCalVal(result);
+        }
+        else {
+            const newDisplayValue = calVal + buttonText;
+            setCalVal(newDisplayValue);
+        }
+    }
+  return(
+      <div className={styles.calculator}>
+        <Display displayValue={ calVal}></Display>
+        <ButtonContainer onButtonClick={onButtonClick}></ButtonContainer>
+
+      </div>
+  );
+}
+
+export default App;
